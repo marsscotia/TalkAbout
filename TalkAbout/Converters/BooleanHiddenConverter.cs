e98334db9b576f8f6c14ee9f9fea0e862820eb45
Mark <Mark@ART_ROOM>
2016-06-22 22:49:11 +0100

    Completed ShortcutPanel User Control

---
+++ TalkAbout/Converters/BooleanHiddenConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace TalkAbout.Converters
+{
+    public class BooleanHiddenConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, string language)
+        {
+
+            Visibility result;
+            if ((bool)value)
+            {
+                result = Visibility.Visible;
+            }
+            else
+            {
+                result = Visibility.Collapsed;
+            }
+            return result;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TalkAbout/View/ShortcutPanel.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using TalkAbout.Model;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace TalkAbout.View
+{
+    public sealed partial class ShortcutPanel : UserControl
+    {
+        public static readonly DependencyProperty CommandLabelProperty =
+            DependencyProperty.Register("CommandLabel", typeof(string), typeof(ShortcutPanel), null);
+
+        public static readonly DependencyProperty CommandShortcutProperty =
+            DependencyProperty.Register("CommandShortcut", typeof(string), typeof(ShortcutPanel), null);
+
+        public static readonly DependencyProperty SymbolTextProperty =
+            DependencyProperty.Register("SymbolText", typeof(string), typeof(ShortcutPanel), null);
+
+        public string CommandLabel
+        {
+            get
+            {
+                return (string)GetValue(CommandLabelProperty);
+            }
+            set
+            {
+                SetValue(CommandLabelProperty, value);
+            }
+        }
+
+        public string CommandShortcut
+        {
+            get
+            {
+                return (string)GetValue(CommandShortcutProperty);
+            }
+            set
+            {
+                SetValue(CommandShortcutProperty, value);
+            }
+        }
+
+        public string SymbolText
+        {
+            get
+            {
+                return (string)GetValue(SymbolTextProperty);
+
+            }
+            set
+            {
+                SetValue(SymbolTextProperty, value);
+            }
+        }
+
+        public bool ShortcutVisible
+        {
+            get
+            {
+                return _settings.ShowShortcuts;
+            }
+            set
+            {
+                _settings.ShowShortcuts = value;
+            }
+        }
+
+        private Model.Settings _settings;
+
+        public ShortcutPanel()
+        {
+            this.InitializeComponent();
+            _settings = Model.Settings.Instance;
+        }
+    }
+}
