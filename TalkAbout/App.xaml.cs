7c1523ca119593f977ae6139426a532b9c3e2f18
Mark <Mark@ART_ROOM>
2018-09-29 16:58:44 +0100

    Disabled frame rate counter during debug

--- TalkAbout/App.xaml.cs
+++ TalkAbout/App.xaml.cs
@@ -50,7 +50,7 @@ namespace TalkAbout
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
-                this.DebugSettings.EnableFrameRateCounter = true;
+                this.DebugSettings.EnableFrameRateCounter = false;
             }
 #endif
             Frame rootFrame = Window.Current.Content as Frame;
